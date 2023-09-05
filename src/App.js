b01833391852b23e2df6764e7039195782d9deba
Linton <pereira.linton@gmail.com>
2023-09-05 14:50:27 +0530

    Updated fLogin form styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="">
-      <Header />
       <Outlet />
     </div>
   );
--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,50 +1,44 @@
 import React, { useState } from "react";
+import Header from "./Header";
 
 const LoginPage = () => {
   const [signUp, setSignUp] = useState(false);
   return (
     <div>
+      <Header />
       <img
         className="absolute"
         alt="Background"
         src="https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
       />
       <form
-        className="absolute bg-black p-2 mx-auto left-0 right-0 my-36 w-3/12 bg-opacity-80 rounded-md  flex flex-col items-center"
+        className="absolute bg-black text-white p-12 mx-auto left-0 right-0 my-32 w-3/12 bg-opacity-80 rounded-md  flex flex-col items-center"
         onSubmit={(e) => e.preventDefault()}
       >
-        <h1 className="font-semibold text-2xl text-white m-5">
+        <h1 className="font-semibold text-2xl m-5">
           {signUp ? "Sign Up" : "Sign In"}
         </h1>
         {signUp && (
-          <div className="my-2 h-12  mx-4">
-            <input
-              className="p-2 bg-slate-600 rounded-md opacity-100 text-white font-medium"
-              type="text"
-              placeholder="Full Name"
-            />
-          </div>
-        )}
-        <div className="my-2 h-12  mx-4">
           <input
-            className="p-2 bg-slate-600 rounded-md opacity-100 text-white font-medium"
+            className="p-3 m-2 w-full bg-slate-600 rounded-md opacity-100 font-medium"
             type="text"
-            placeholder="Email or phone number"
+            placeholder="Full Name"
           />
-        </div>
-        <div className="my-2 h-12  mx-4">
-          <input
-            className="p-2 bg-slate-600 rounded-md opacity-100 text-white font-medium"
-            type="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="my-2 h-12  mx-4">
-          <button className="bg-red-600 m-2 p-2 rounded-md text-white font-semibold  opacity-100 ">
-            {signUp ? "Sign Up" : "Sign In"}
-          </button>
-        </div>
-        <div className="mt-4 place-content-start">
+        )}
+        <input
+          className="p-3 m-2 w-full bg-slate-600 rounded-md opacity-100 font-medium"
+          type="text"
+          placeholder="Email or phone number"
+        />
+        <input
+          className="p-3 m-2 w-full bg-slate-600 rounded-md opacity-100 font-medium"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="bg-red-600 my-6 w-full p-2 rounded-md font-semibold  opacity-100">
+          {signUp ? "Sign Up" : "Sign In"}
+        </button>
+        <div className="mt-4 opacity-100">
           <p className="text-slate-500">
             {signUp ? "Have an account?  " : "New to Netflix?  "}
             <span
