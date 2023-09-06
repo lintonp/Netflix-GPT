63a26ad1d9b083c7a801886ee93c4e00810dea8e
Linton <pereira.linton@gmail.com>
2023-09-07 00:06:40 +0530

    firebase initial setup

---
+++ src/Utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqy3OIGwOWkU-7t64yV6NcehoKwroDmHo",
+  authDomain: "netflixgpt-8c5a9.firebaseapp.com",
+  projectId: "netflixgpt-8c5a9",
+  storageBucket: "netflixgpt-8c5a9.appspot.com",
+  messagingSenderId: "165823922092",
+  appId: "1:165823922092:web:1bb229ea0db90d52234409",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
